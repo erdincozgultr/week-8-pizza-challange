d781630bf1f71c479383a7fede052af7ac76805f
Erdinç ÖZGÜL <erdincozgul.tr@gmail.com>
2025-07-25 14:14:34 +0300

    iteration-1-banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import './App.css'
+import Banner from './components/banner'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Banner/>
     </>
   )
 }
+
 
 export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./banner.css"
+export default function Banner () {
+    return (<>
+        <section className="banner">
+            <div className="container">
+                <img className="logo" src="../images/iteration-1-images/logo.svg"/>
+                <div className="banner-content">
+                    <h1>KOD ACIKTIRIR <br/> PIZZA, DOYURUR </h1>
+                    <button className="btn">ACIKTIM</button>
+                </div>
+            </div>
+   </section>
+    </>)
+}
